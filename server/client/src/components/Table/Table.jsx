5e6846ee5f346311c57328bbee3ac6a5d44a2037
Glen Carpenter <glen@glencarpenter.io>
2020-10-03 21:39:06 -0600

    heroku build script

--- server/client/src/components/Table/Table.jsx
+++ server/client/src/components/Table/Table.jsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import { paginateArray } from "../../utils";
 import { Paginator } from "../";
@@ -11,13 +11,7 @@ const Table = props => {
   const [page, setPage] = useState(0);
   // Keys for headers and columns
   const keys = ["name", "city", "state", "telephone", "genre"];
-
-  const [paginatedData, setPaginatedData] = useState(null);
-
-  useEffect(() => {
-    console.log("Made it");
-    setPaginatedData(paginateArray(data, 10));
-  }, [data]);
+  const paginatedData = paginateArray(data, 10);
 
   const containerCss = css`
     min-height: 50vh;
